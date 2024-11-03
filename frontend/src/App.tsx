d828e811ab1d856b2879c4306f8424a168f40d12
harry-lons <harrylonsdale25@gmail.com>
2024-11-02 17:12:55 -0700

    install browser router and setup basic pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LoginSignup from './pages/LoginSignup.tsx';
-import Events from './pages/Events.tsx';
-import DetailedEvent from './pages/DetailedEvent.tsx';
+import LoginSignup from './pages/LoginSignup';
+import Events from './pages/Events';
+import DetailedEvent from './pages/DetailedEvent';
 import './App.css';
 
 function App() {
@@ -11,6 +11,7 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<LoginSignup />} />
+          <Route path="/login" element={<LoginSignup />} />
           <Route path="/events" element={<Events />} />
           <Route path="/events/:id" element={<DetailedEvent />} />
         </Routes>
