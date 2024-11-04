bdb97cdde236b38dbe01a78bb0113f3192be9292
ShengqiWu2004 <114774291+ShengqiWu2004@users.noreply.github.com>
2024-11-03 18:13:53 -0800

    Type Script Added, name of DetailedEvent change to UserDetailedEvent

---
+++ frontend/src/types/types.ts
@@ -0,0 +1,33 @@
+export type Event =
+{
+	id: string;
+	club_id : string;
+	location: string;
+	time: Date;
+	summary: string;
+	details: { [key: string]: any };
+	type: string;
+};
+
+export type RSVP = 
+{
+	user_id: string;
+	event_id: string;
+};
+
+export type User = 
+{
+	id: string;
+	username: string;
+	first_name: string;
+	last_name: string;
+	followed_clubs: string[];
+};
+
+export type Club =
+{
+	id : string;
+	name: string;
+	board_members: string[];
+	contact_email: string[];
+};
