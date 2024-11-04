57ddba07bcc1aac753957ee7b3d0fd368f245aef
Gamepr023 <122562465+Gamepr023@users.noreply.github.com>
2024-11-04 11:56:30 -0800

    basic webpage with MUI errors

--- frontend/src/pages/DetailedEvent.tsx
+++ frontend/src/pages/DetailedEvent.tsx
@@ -1,7 +1,12 @@
-export const DetailedEvent = () => {
+import { Event } from "../types/types";
+
+export const DetailedEvent = (currentEvent: Event) => {
     return (
-        <div>
-            <h1>Detailed Event page</h1>
+        <div className="event-detail">
+            <div className="Description">{currentEvent.summary}</div>
+            <div className="Time">{currentEvent.time.toString()}</div>
+            <div className="Location">{currentEvent.location}</div>
+            <div className="EventType">{currentEvent.type}</div>
         </div>
     );
 };
