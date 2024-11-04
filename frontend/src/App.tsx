f355f17a708cb6f92a478675a78bc1088970cb06
Gamepr023 <122562465+Gamepr023@users.noreply.github.com>
2024-11-04 11:56:30 -0800

    basic webpage with MUI errors

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Router, Routes, Route } from 'react-router-dom';
 import { LoginSignup } from './pages/LoginSignup';
-import { Events } from './pages/Events';
+import { Events } from './pages/Events/Events';
 import { DetailedEvent } from './pages/DetailedEvent';
 import './App.css';
 
@@ -11,7 +11,7 @@ function App() {
           <Route path="/" element={<LoginSignup />} />
           <Route path="/login" element={<LoginSignup />} />
           <Route path="/events" element={<Events />} />
-          <Route path="/events/:id" element={<DetailedEvent />} />
+          <Route path="/events/:id" element={<DetailedEvent id="1" club_id="Club A" title="Club A Meeting" location="Price Center" time={new Date( 2024, 10, 4, 18, 30, 0, 0)} summary="text" details={["1"]} type="event"/>} />
       </Routes>
     </div>
   );
