6598e377d7cd7d61e7c2e66de9a6fffefeb7fa89
harry-lons <harrylonsdale25@gmail.com>
2024-11-03 23:32:53 -0800

    MVP UI for login/signup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LoginSignup from './pages/LoginSignup';
+import LoginSignup from './pages/LoginSignup/LoginSignup';
 import Events from './pages/Events';
 import DetailedEvent from './pages/DetailedEvent';
 import './App.css';
@@ -10,8 +10,9 @@ function App() {
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/" element={<LoginSignup />} />
-          <Route path="/login" element={<LoginSignup />} />
+          <Route path="/" element={<LoginSignup which="LOG IN"/>} />
+          <Route path="/login" element={<LoginSignup which="LOG IN"/>} />
+          <Route path="/signup" element={<LoginSignup which="SIGN UP" />} />
           <Route path="/events" element={<Events />} />
           <Route path="/events/:id" element={<DetailedEvent />} />
         </Routes>
