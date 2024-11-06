719b55ac5db350eba5642139f6fcd2b4e8e8905c
Gamepr023 <122562465+Gamepr023@users.noreply.github.com>
2024-11-06 00:09:16 -0800

    rebase with main b21853f

--- frontend/src/constants/constants.tsx
+++
@@ -1,8 +0,0 @@
-export const dummyEventsList = [
-    { time: new Date("2024-10-28T17:00:00"), title: "Event Name 1", club_id: "Club 1", location: "CSE Building" },
-    { time: new Date("2024-10-28T18:00:00"), title: "Event Name 2", club_id: "Club 2", location: "Price Center" },
-    { time: new Date("2024-10-29T18:00:00"), title: "Event Name 3", club_id: "Club 3", location: "Geisel" },
-    { time: new Date("2024-10-30T18:30:00"), title: "Event Name 4", club_id: "Club 4", location: "Library" },
-    { time: new Date("2024-10-30T18:45:00"), title: "Event Name 5", club_id: "Club 5", location: "Student Center" },
-    { time: new Date("2024-11-01T18:00:00"), title: "Event Name 6", club_id: "Club 6", location: "Recreation Center" }
-  ];
