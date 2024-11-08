f5eaa0bf434b2c2a08f7ab0e51578cc2dcd53f04
harry-lons <harrylonsdale25@gmail.com>
2024-11-07 19:57:25 -0800

    login auth flow v1

--- frontend/src/pages/LoginSignup/LoginSignupCard.tsx
+++ frontend/src/pages/LoginSignup/LoginSignupCard.tsx
@@ -1,18 +1,71 @@
-import React from 'react';
-
-import { Grid, Card, CardContent, TextField, InputAdornment, IconButton, OutlinedInput, Button } from '@mui/material';
+import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Card, CardContent, TextField, InputAdornment, IconButton, OutlinedInput, Button } from '@mui/material';
 import { Visibility, VisibilityOff } from '@mui/icons-material';
+import { AuthContext } from '../../context/AuthContext';
 
 interface LoginSignupCardProps {
     which?: string; // Define whether this is a login card or signup card
 }
 const LoginSignupCard: React.FC<LoginSignupCardProps> = ({ which }) => {
     const [showPassword, setShowPassword] = React.useState(false);
+    const [enteredUsername, setEnteredUsername] = React.useState("");
+    const [enteredPassword, setEnteredPassword] = React.useState("");
+    const { saveToken } = useContext(AuthContext);
+    const navigate = useNavigate();
 
     const handleClickShowPassword = () => setShowPassword((show) => !show);
 
     const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
         event.preventDefault();
+    };
+
+    const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setEnteredUsername(event.target.value);
+    };
+
+    const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setEnteredPassword(event.target.value);
+    };
+
+    const handleSubmitForm = async (event: React.MouseEvent<HTMLButtonElement>) => {
+        let endpointURL = process.env.REACT_APP_BACKEND_URL;
+        if (!endpointURL) {
+            console.error('Backend URL is not defined');
+            return;
+        }
+        // Create form-data from state
+        const formData = new FormData();
+        formData.append('username', enteredUsername);
+        formData.append('password', enteredPassword);
+
+        // Set the request url to be backend/token
+        const tokenURL = `${endpointURL}/token`;
+        try {
+            const response = await fetch(tokenURL, {
+                method: 'POST',
+                body: formData
+            });
+
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+
+            const data = await response.json();
+            console.log('Response:', data);
+
+            // Check if the response contains a token
+            if (data.access_token) {
+                console.log('Token received:', data.access_token);
+                saveToken(data.access_token);  // Store the token in context
+                navigate('/events');     // Redirect to /events page
+            } else {
+                console.error('No token found in the response');
+            }
+
+        } catch (error) {
+            console.error('There was a problem with the fetch operation:', error);
+        }
     };
 
     const handleMouseUpPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -31,6 +84,7 @@ const LoginSignupCard: React.FC<LoginSignupCardProps> = ({ which }) => {
                         variant="outlined"
                         fullWidth
                         type="email"
+                        onChange={handleUsernameChange}
                     />
                 </div>
 
@@ -42,6 +96,8 @@ const LoginSignupCard: React.FC<LoginSignupCardProps> = ({ which }) => {
                         id="outlined-adornment-password"
                         fullWidth
                         type={showPassword ? 'text' : 'password'}
+                        value={enteredPassword}
+                        onChange={handlePasswordChange}
                         endAdornment={
                             <InputAdornment position="end">
                                 <IconButton
@@ -63,6 +119,7 @@ const LoginSignupCard: React.FC<LoginSignupCardProps> = ({ which }) => {
                     variant="contained"
                     fullWidth
                     id='logsign-submit-button'
+                    onClick={handleSubmitForm}
                 >
                     {which}
                 </Button>
