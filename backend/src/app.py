09ad9bc71bd7fd2139dd36302f4d9549ba3c9caa
Ali Alabiad <ali.z03@yahoo.com>
2024-11-10 23:46:18 -0800

    initial setup of user signup endpoint
    current implementation is on fake database.
    added the scripts and a couple of  TODOs

--- backend/src/app.py
+++ backend/src/app.py
@@ -5,7 +5,36 @@ from .authentication.service import app as auth_router
 from .events.service import app as event_router
 from .identities.service import app as identities_router
 
-app = FastAPI()
+from .db_store.models import *
+from .db_store.postgres import PostgresDatabase
+from sqlalchemy import create_engine, text
+from sqlalchemy.engine import URL
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
+from contextlib import contextmanager
+
+url = URL.create(
+    drivername="postgresql+psycopg",
+    username="username",
+    password="examplepassword",
+    host="127.0.0.1",
+    port=5432,
+    database="SoCalSocial",
+)
+
+engine = create_engine(url)
+conn = engine.connect()
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+@contextmanager
+def start_db(app: FastAPI):
+    db = PostgresDatabase(session=session)
+    yield
+    print("Closing databse connection")
+
+app = FastAPI(lifespan=start_db)
 app.include_router(auth_router)
 app.include_router(event_router)
 app.include_router(identities_router)
