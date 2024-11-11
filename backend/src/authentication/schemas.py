4f01d4e88b98bde439f500d5170263a775821f38
Ali Alabiad <ali.z03@yahoo.com>
2024-11-10 23:46:18 -0800

    initial setup of user signup endpoint
    current implementation is on fake database.
    added the scripts and a couple of  TODOs

--- backend/src/authentication/schemas.py
+++ backend/src/authentication/schemas.py
@@ -6,7 +6,7 @@ class BearerToken(BaseModel):
     access_token: str
     token_type: str
 
-
+## TODO: merge the UserLogin and UserSignup to one class
 class UserLogin(User):
     # id: int
     # username: str
@@ -14,6 +14,29 @@ class UserLogin(User):
     # last_name: str
     hashed_password: str
 
+    def to_user(self) -> User:
+        # Returns with everything but hashed_password
+        user_dict = self.model_dump()
+        if isinstance(user_dict.get("hashed_password"), str):
+            del user_dict["hashed_password"]
+        else:
+            raise ValueError("UserLogin object does not contain a password")
+        return User(**user_dict)
+
+'''
+creating a class to handle user sign ins including the following
+member variables:
+- first_name: user's first name
+- last_name: user's last name
+- username: profile username
+- password: password
+'''
+class UserSignup(User):
+    first_name: str
+    last_name: str
+    username: str
+    password:str
+    
     def to_user(self) -> User:
         # Returns with everything but hashed_password
         user_dict = self.model_dump()
