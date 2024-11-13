bae856acb70c0c13afd99ee6ed25fb8e24f4ea75
harry-lons <harrylonsdale25@gmail.com>
2024-11-13 14:56:09 -0800

    changed hard-coded backend link in constants to use .env

--- frontend/src/constants/constants.ts
+++ frontend/src/constants/constants.ts
@@ -1,5 +1,5 @@
 import { Event, Club,User,EventType } from "../types/types";
-export const API_BASE_URL = "http://localhost:8000";
+export const API_BASE_URL = process.env.REACT_APP_BACKEND_URL;
 export const exampleEvent = {
 	id: "001",
     title: "Creative Writing Workshop: Unleash Your Imagination!",
