9be07bbe0b17942b853e8ce811e1f90c8ef083ce
ShengqiWu2004 <114774291+ShengqiWu2004@users.noreply.github.com>
2024-11-14 20:52:54 -0800

    More navigation added

--- frontend/src/pages/UserProfile/EventCard.tsx
+++ frontend/src/pages/UserProfile/EventCard.tsx
@@ -6,27 +6,30 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-
+import { useNavigate } from "react-router-dom";
 interface EventCardProps {
     user: User;
     events: Event[];
 }
 export const EventCard: React.FC<EventCardProps> = ({ user,events}) =>{
     const today = new Date();
-    
+    const navigate = useNavigate();
+    const goToDetailPage = (event_id: string)=> {
+        navigate(`/events/${event_id}`);
+    }  
     return(
     <Card sx={{ width: '100%', height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between',
         backgroundColor: 'rgba(255, 255, 255, 0.05)',overflowY: 'auto'}}>
         
         <CardContent className="event-details-column">
             {events.map((event) => (
-                <div className="event-info" onClick={() => {console.log(event.club_id)}}>
-                    <div className="event-time">
+                <div className="past-event-info" onClick={() => {console.log(event.club_id)}}>
+                    <div className="past-event-time">
                         {event.begin_time.toLocaleDateString([], { year: 'numeric', month: '2-digit', day: '2-digit' })}
                     </div>
-                    <div className="event-details">
-                        <p className="event-title">{event.title}</p>
-                        <p className="event-club-location">{event.club_id}, {event.location}</p>
+                    <div className="past-event-details">
+                        <p className="past-event-title" onClick={()=>goToDetailPage(event.id)}>{event.title}</p>
+                        <p className="past-event-club-location">{event.club_id}, {event.location}</p>
                     </div>
                 </div>
             ))}
