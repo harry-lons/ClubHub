12eca2dd545d4539e23ccd3a517f5b51d59ddc99
zlzcty <zlzcty@example.com>
2024-11-19 14:13:22 -0800

    Add user login tests

--- backend/tests/test_authentication.py
+++ backend/tests/test_authentication.py
@@ -1,7 +1,15 @@
+from datetime import timedelta
+
 import pytest
+from fastapi import HTTPException
 from fastapi.testclient import TestClient
 
 from src.app import app, start_db
+from src.authentication.service import (
+    ACCESS_TOKEN_EXPIRE_MINUTES,
+    authenticate_user2,
+    create_access_token,
+)
 
 # from .utils import client
 
@@ -17,7 +25,29 @@ def test_login_token(client):
 
 def test_login_token_bad(client):
     """The login should fail with invalid credientials"""
+    # with pytest.raises(HTTPException) as exc_info:
     response = client.post(
         "/user/login", data={"username": "fake_username", "password": ""}
     )
     assert response.status_code == 401
+    # assert isinstance(exc_info.value, HTTPException)
+    # assert exc_info.value.status_code == 404
+    # assert exc_info.value.detail == "Email does not exist"
+
+
+def test_user_token(client):
+    user = authenticate_user2("username1@example.com", "password")
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username, "role": "user"}, expires_in=access_token_expires
+    )
+    response = client.get(
+        "/user/whoami", headers={"Authorization": f"Bearer {access_token}"}
+    )
+    assert response.status_code == 200
+    response_data = response.json()
+    assert len(response_data) > 0, "Response is empty"
+
+    assert response_data["username"] == "username1@example.com" == user.username
+    assert response_data["first_name"] == user.first_name
+    assert response_data["last_name"] == user.last_name
