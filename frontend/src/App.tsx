524588722e814877cca5855e910ba9670d5e7d20
a-nguy4n <1512alli@gmail.com>
2024-12-02 12:58:41 -0800

    Updating

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -43,7 +43,7 @@ function App() {
             <Route path="/clubs" element={<Clubs/>} />
             <Route path = "/clubDetail/:id" element = {<ClubDetail which="USER"/>}/>
             <Route path = "/club/clubDetail/:id" element = {<ClubDetail which="CLUB"/>}/>
-            <Route path = "/club/profile" element = {<ClubDetail which = "CLUB"/>}/>
+            <Route path = "/club/profile" element = {<ClubProfile which="CLUB"/>}/>
             <Route path = "/homepage" element = {<HomePage/>}/>
             {/* <Route path = "/testpage" element = {<TestPage/>}/> */}
             <Route path="/club/events" element={<ClubEventList which="CLUB"/>} />
