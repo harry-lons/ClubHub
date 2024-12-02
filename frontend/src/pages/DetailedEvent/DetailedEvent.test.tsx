8ea75bacd8e66da992ecda27f756b7d9939984f8
readyvivian <122570273+readyvivian@users.noreply.github.com>
2024-12-02 01:41:35 -0800

    Finish Detailed Event Tests

--- frontend/src/pages/DetailedEvent/DetailedEvent.test.tsx
+++ frontend/src/pages/DetailedEvent/DetailedEvent.test.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
-import { BrowserRouter,useParams } from 'react-router-dom';
+import { BrowserRouter,useParams,useNavigate } from 'react-router-dom';
 import DetailedEvent from './DetailedEvent';
 import * as eventUtils from '../../utils/event-utils';
 import * as clubUtils from '../../utils/club-utils';
 import * as RSVPUtils from '../../utils/RSVP-utils';
 import { Event,Club,User,EventType } from "../../types/types";
 import { AuthContext } from '../../context/AuthContext';
+import { fetchCurrentAttendees } from '../../utils/RSVP-utils';
+import { cleanup,act } from '@testing-library/react';
 
 
 jest.mock('../../utils/event-utils');
@@ -21,6 +23,7 @@ jest.mock('../../utils/RSVP-utils', () => ({
 jest.mock('react-router-dom', () => ({
     ...jest.requireActual('react-router-dom'),
     useParams: jest.fn(),
+    useNavigate: jest.fn(),
   }));
 
   
@@ -65,7 +68,7 @@ describe('User-side DetailedEvent Component', () => {
       await waitFor(() => expect(eventUtils.fetchEventById).toHaveBeenCalled());
     });
 
-    test('rendering the event title', async () => {
+    test('rendering event title', async () => {
         (useParams as jest.Mock).mockReturnValue({ id: '1' });
       render(
         <BrowserRouter>
@@ -88,7 +91,7 @@ describe('User-side DetailedEvent Component', () => {
       await waitFor(() => expect(clubUtils.fetchClubById).toHaveBeenCalled());
     });
 
-    test('rendering the club name', async () => {
+    test('rendering club name', async () => {
         (useParams as jest.Mock).mockReturnValue({ id: '1' });
       render(
         <BrowserRouter>
@@ -230,5 +233,163 @@ describe('User-side DetailedEvent Component', () => {
         expect(updatedButton).toBeInTheDocument();
         expect(updatedButton).toHaveTextContent('RSVP');
       });
+  });
+
+  describe('Club-side DetailedEvent Component', () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+      jest.spyOn(eventUtils, 'fetchEventById').mockResolvedValue({
+      id: "1",
+      title: "Creative Writing Workshop: Unleash Your Imagination!",
+      club_id : "001",
+      location: "Geisel",
+      begin_time: new Date(2024, 11, 1, 10, 0, 0),
+      end_time: new Date(2024, 11, 1, 11, 0, 0),
+      recurrence: false,
+      recurrence_type: null,
+      stop_date: null,
+      summary: "Join us for an engaging and inspiring Creative Writing Workshop hosted by The Literary Society! Whether you're a seasoned writer or just starting your journey, this workshop is designed to spark your creativity and help you develop your writing skills. Participants will explore various writing prompts, learn about character development, and receive constructive feedback on their work. This is a fantastic opportunity to meet fellow writers, share ideas, and enhance your craft in a supportive environment. Bring your favorite notebook and pen, and let your imagination run wild! Refreshments will be provided. Reserve your spot today!",
+      pictures: ["flyer.jpg"],
+      type: ["social", "workshop"] as EventType[],
+      capacity: null
+	  });
+      jest.spyOn(clubUtils, 'fetchClubById').mockResolvedValue({
+		  id : "001",
+	    name: "The Literary Society",
+	    board_members: ["Vivian Wang", "Shengqi Wu", "Harry", "Allen", "Peter", "George", "Ali", "Allison"],
+	    contact_email: ["contact@example.com"],
+      description: "The Literary Society is a vibrant community dedicated to the love of literature, fostering an environment where members can explore, discuss, and celebrate the written word. Led by a passionate team, including the dedicated board member Member 1, the club serves as a hub for book enthusiasts, writers, and creatives alike.For inquiries or more information, feel free to reach out to us at contact@example.com. Join us to connect with like-minded individuals, participate in engaging events, and deepen your appreciation for the world of literature!"
+	  });
+    jest.spyOn(clubUtils, 'fetchClubWho').mockResolvedValue({
+		  id : "001",
+	    name: "The Literary Society",
+	    board_members: ["Vivian Wang", "Shengqi Wu", "Harry", "Allen", "Peter", "George", "Ali", "Allison"],
+	    contact_email: ["contact@example.com"],
+      description: "The Literary Society is a vibrant community dedicated to the love of literature, fostering an environment where members can explore, discuss, and celebrate the written word. Led by a passionate team, including the dedicated board member Member 1, the club serves as a hub for book enthusiasts, writers, and creatives alike.For inquiries or more information, feel free to reach out to us at contact@example.com. Join us to connect with like-minded individuals, participate in engaging events, and deepen your appreciation for the world of literature!"
+	  });
+    (fetchCurrentAttendees as jest.Mock).mockResolvedValue([
+      {
+        id: '123',
+        username: 'johndoe',
+        first_name: 'John',
+        last_name: 'Doe',
+        followed_clubs: ['001', '002'],
+      },
+      {
+        id: '456',
+        username: 'janesmith',
+        first_name: 'Jane',
+        last_name: 'Smith',
+        followed_clubs: ['001'],
+      },
+    ]);
+    (useParams as jest.Mock).mockReturnValue({ id: '1' });
+    });
+  
+    afterEach(() => {
+      jest.clearAllMocks();
+      cleanup();
+    });
+  
+    test('calling fetchEventById', async () => {
+      render(
+        <BrowserRouter>
+          <DetailedEvent which="CLUB" />
+        </BrowserRouter>
+      );
+      await waitFor(() => expect(eventUtils.fetchEventById).toHaveBeenCalled());
+    });
+
+    test('rendering event title', async () => {
+      render(
+        <BrowserRouter>
+          <DetailedEvent which="CLUB" />
+        </BrowserRouter>
+      );
+      const titleElement = await screen.findByText(
+        'Creative Writing Workshop: Unleash Your Imagination!'
+      );
+      expect(titleElement).toBeInTheDocument();
+    });
+
+    test('calling fetchClubById', async () => {
+      render(
+        <BrowserRouter>
+          <DetailedEvent which="CLUB" />
+        </BrowserRouter>
+      );
+      await waitFor(() => expect(clubUtils.fetchClubById).toHaveBeenCalled());
+    });
+
+    test('rendering club name', async () => {
+      render(
+        <BrowserRouter>
+          <DetailedEvent which="CLUB" />
+        </BrowserRouter>
+      );
+      const clubNameElement = await screen.findByText(
+        'The Literary Society'
+      );
+      expect(clubNameElement).toBeInTheDocument();
+    });
+
+    test('rendering Edit button', async () => {
+
+        const mockAuthContext = {
+          token: 'fake-token',
+          id: '001',
+          accountType: 'club',
+          saveToken: jest.fn(),
+          setAccountType: jest.fn(),
+          setId: jest.fn(),
+          removeToken: jest.fn(),
+        };
+
+        await act(async () => {
+          render(
+            <AuthContext.Provider value={mockAuthContext}>
+              <BrowserRouter>
+                <DetailedEvent which="CLUB" />
+              </BrowserRouter>
+            </AuthContext.Provider>
+          );
+        });
+
+        const editButton = await waitFor(() => screen.getByRole('button', { name: /Edit/i }));
+        expect(editButton).toBeInTheDocument();
+        expect(editButton).toHaveTextContent('Edit');
+    });
+
+    test('clicking Edit button', async () => {
+        const mockNavigate = jest.fn();
+        (useNavigate as jest.Mock).mockReturnValue(mockNavigate);
+
+        const mockAuthContext = {
+          token: 'fake-token',
+          id: '001',
+          accountType: 'club',
+          saveToken: jest.fn(),
+          setAccountType: jest.fn(),
+          setId: jest.fn(),
+          removeToken: jest.fn(),
+        };
+    
+        await act(async () => {
+          render(
+            <AuthContext.Provider value={mockAuthContext}>
+              <BrowserRouter>
+                <DetailedEvent which="CLUB" />
+              </BrowserRouter>
+            </AuthContext.Provider>
+          );
+        });
+
+        const editButton = await waitFor(() => screen.getByRole('button', { name: /Edit/i }));
+        expect(editButton).toBeInTheDocument();
+        expect(editButton).toHaveTextContent('Edit');
+        
+        fireEvent.click(editButton);
+        expect(mockNavigate).toHaveBeenCalledWith(`/club/editEvent/1`);
+    });
   });
   
